9962786089ddfa0e6350fd8a2b4da0c79aecca92
zhiming.tang <zhiming.tang@tcl.com>
2013-12-12 18:41:43 +0800

    support multiple object drawing

--- t3d.js
+++ t3d.js
@@ -13,73 +13,43 @@ var t3d = {
 
     return glContext;
   },
-
-  setupProgram: function(aVertexShaderSrc, aFragmentShaderSrc) {
-    if (!glContext) {
-      throw Error("WebGL context isn't created before setup shader!");
+  
+  startDraw: function(){
+    if (null === glContext){
+      throw Error("Must init before startDraw()");
     }
 
-    if (!aVertexShaderSrc || !aFragmentShaderSrc) {
-      throw Error('Shader can not be empty!');
-    }
-
-    var program = glContext.createProgram();
-    var vShader = glContext.createShader(glContext.VERTEX_SHADER);
-    var fShader = glContext.createShader(glContext.FRAGMENT_SHADER);
-
-    glContext.shaderSource(vShader, aVertexShaderSrc);
-    glContext.shaderSource(fShader, aFragmentShaderSrc);
-
-    glContext.compileShader(vShader);
-    glContext.compileShader(fShader);
-
-    if (!glContext.getShaderParameter(vShader, glContext.COMPILE_STATUS)) {
-      throw Error(glContext.getShaderInfoLog(vShader));
-    }
-
-    if (!glContext.getShaderParameter(fShader, glContext.COMPILE_STATUS)) {
-      throw Error(glContext.getShaderInfoLog(fShader));
-    }
-
-    glContext.attachShader(program, vShader);
-    glContext.attachShader(program, fShader);
-    glContext.linkProgram(program);
-
-    if (!glContext.getProgramParameter(program, glContext.LINK_STATUS)) {
-      throw Error('Could not link the shader');
-    }
-
-    return program;
+    glContext.clearColor(0.0, 0.0, 0.0, 1.0);
+    glContext.clear(glContext.COLOR_BUFFER_BIT);
   },
 
   getGL: function() {
     return glContext;
   },
 
-  Matrix: function() {
-    this.mat = [1, 0, 0, 0,
-                0, 1, 0, 0,
-                0, 0, 1, 0,
-                0, 0, 0, 1];
-  },
-
-
   //XXX the private items/function should be protected in someway
   glContext: null
 
 };
 
-t3d.Matrix.prototype = {
-  /*We use a normal array (with 16 elements) to represent a 4x4 matrix*/
+function Matrix() {
+  this.__mat = [1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1];
+}
+
+Matrix.prototype = {
+  /*We use a normal array (with 16 elements) to represent a 4x4.__matrix*/
 
     __multiplyM: function(aMatrixA) {
-      var orig = this.mat.slice(0,16);
+      var orig = this.__mat.slice(0,16);
 
       for (var i = 0; i < 4; i++) {
         for (var j = 0; j < 4; j++) {
-          this.mat[i + j * 4] = 0;
+          this.__mat[i + j * 4] = 0;
           for (var k = 0; k < 4; k++) {
-            this.mat[i + j * 4] += aMatrixA[i + k * 4] * orig[k + j * 4];
+            this.__mat[i + j * 4] += aMatrixA[i + k * 4] * orig[k + j * 4];
           }
         }
       }
@@ -87,11 +57,11 @@ t3d.Matrix.prototype = {
 
     transpose: function() {
       for (var i = 0; i < 4; i++) {
-        this.mat[i * 4 + i] = this.mat[i * 4 + i];
+        this.__mat[i * 4 + i] = this.__mat[i * 4 + i];
 
         for (var j = 0; j < i; j++) {
-          this.mat[i * 4 + j] = this.mat[j * 4 + i];
-          this.mat[j * 4 + i] = this.mat[i * 4 + j];
+          this.__mat[i * 4 + j] = this.__mat[j * 4 + i];
+          this.__mat[j * 4 + i] = this.__mat[i * 4 + j];
         }
       }
 
@@ -183,9 +153,175 @@ t3d.Matrix.prototype = {
     },
 
     toGLArray: function() {
-      return new Float32Array(this.mat);
-    }
-  
+      return new Float32Array(this.__mat);
+    } 
 };
 
+function DrawObject() {
+  this.__program = null;
+  this.__VBO = [];
+  this.__index = {};
+  this.__uniforms = [];
+}
 
+DrawObject.prototype = {
+
+  setupProgram: function(aVertexShaderSrc, aFragmentShaderSrc) {
+    if (null === t3d.getGL()) {
+      throw Error("WebGL context isn't created before setup shader!");
+    }
+
+    if (!aVertexShaderSrc || !aFragmentShaderSrc) {
+      throw Error('Shader can not be empty!');
+    }
+
+    var gl = t3d.getGL();
+
+    var program = gl.createProgram();
+    var vShader = gl.createShader(gl.VERTEX_SHADER);
+    var fShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+    gl.shaderSource(vShader, aVertexShaderSrc);
+    gl.shaderSource(fShader, aFragmentShaderSrc);
+
+    gl.compileShader(vShader);
+    gl.compileShader(fShader);
+
+    if (!gl.getShaderParameter(vShader, gl.COMPILE_STATUS)) {
+      throw Error(gl.getShaderInfoLog(vShader));
+    }
+
+    if (!gl.getShaderParameter(fShader, gl.COMPILE_STATUS)) {
+      throw Error(gl.getShaderInfoLog(fShader));
+    }
+
+    gl.attachShader(program, vShader);
+    gl.attachShader(program, fShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      throw Error('Could not link the shader');
+    }
+    
+    this.__program = program;
+    return this;
+  },
+
+  addVertexData: function(aVariable, aLen, aBuffer) {
+    this.__check();
+
+    var gl = t3d.getGL();
+    var VBOItem = {len: aLen};
+
+    VBOItem.pos = gl.getAttribLocation(this.__program, aVariable);
+    if (VBOItem.pos < 0){
+      throw Error("Can not find attribute:"+aVariable);
+    }
+    
+    VBOItem.buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, VBOItem.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(aBuffer), gl.STATIC_DRAW);
+
+    VBOItem.type = gl.FLOAT;
+
+    this.__VBO[this.__VBO.length] = VBOItem;
+  },
+
+  addVertexIndex: function(aBuffer){
+    this.__check();
+
+    var gl = t3d.getGL();
+
+    if (this.__index.buffer) {
+      gl.deleteBuffer(this.__index.buffer);
+    }
+
+    this.__index.buffer = gl.createBuffer();
+    this.__index.num = aBuffer.length;
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.__index.buffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(aBuffer), gl.STATIC_DRAW);
+  },
+
+  draw: function() {
+    this.__check();
+
+    var gl = t3d.getGL();
+    gl.useProgram(this.__program);
+
+    for (var i=0; i<this.__VBO.length; i++){
+      var VBOItem = this.__VBO[i];
+
+      gl.enableVertexAttribArray(VBOItem.pos);
+      gl.bindBuffer(gl.ARRAY_BUFFER, VBOItem.buffer);
+      gl.vertexAttribPointer(VBOItem.pos, VBOItem.len, VBOItem.type, false, 0, 0);
+    }
+    
+    for (i=0; i<this.__uniforms.length; i++){
+      var item = this.__uniforms[i];
+
+      if (0 == "matrix".localeCompare(item.type)){
+        if (4 === item.len){
+          gl.uniformMatrix4fv(item.pos, false, item.data.toGLArray());
+        }
+        else {
+          throw Error("Not support");
+        }
+      }
+      else if (0 == "float".localeCompare(item.type)){
+        if (4 == item.len){
+          gl.uniform4fv(item.pos, new Float32Array(item.data));
+        }
+        else {
+          throw Error("Not support");
+        }
+      }
+      else {
+        throw Error("Not support, type="+item.type);
+      }
+    }
+
+    if(!this.__index.buffer){
+      throw Error("Only support index draw now");
+    }
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.__index.buffer);
+    gl.drawElements(gl.TRIANGLES, this.__index.num, gl.UNSIGNED_SHORT, 0);
+    //gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  },
+
+  addUniform: function(aVariable, aType, aLen, aArray) {
+    this.__check();
+
+    var gl = t3d.getGL();
+    var position = gl.getUniformLocation(this.__program, aVariable); 
+    if (!position){
+      throw Error("Can not find uniform:"+aVariable);
+    }
+
+    for(var i=0; i<this.__uniforms.length; i++){
+      var item = this.__uniforms[i];
+
+      if (item.name == aVariable){
+        this.__uniforms.splice(i,1);
+        break;
+      }
+    }
+
+    this.__uniforms[this.__uniforms.length] = {name: aVariable, pos: position, type: aType.toLowerCase(), len: aLen, data: aArray};
+  },
+
+  __getProgram: function(){
+    return this.__program;
+  },
+
+  __check: function() {
+    if (null === t3d.getGL()) {
+      throw Error("WebGL context isn't created !");
+    }
+    
+    if (null === this.__program) {
+      throw Error("Program is not created before op");
+    }
+  }
+};
